2702b374a1c469b00704661b90292757f7e24156
Ezequiel <97308027+Zeky84@users.noreply.github.com>
2024-01-28 20:54:32 -0500

    Initial commit

---
+++ src/main/java/finalproject/ss3v2/Ss3v2Application.java
@@ -0,0 +1,13 @@
+package finalproject.ss3v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ss3v2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ss3v2Application.class, args);
+	}
+
+}
---
+++ src/test/java/finalproject/ss3v2/Ss3v2ApplicationTests.java
@@ -0,0 +1,13 @@
+package finalproject.ss3v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ss3v2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
