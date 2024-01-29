2951fe8293abafba81154c598638bbb48d175dcc
Ezequiel <97308027+Zeky84@users.noreply.github.com>
2024-01-28 20:54:32 -0500

    Initial commit

---
+++ src/main/java/finalproject/ss3v2/dao/request/RefreshTokenRequest.java
@@ -0,0 +1,5 @@
+package finalproject.ss3v2.dao.request;
+
+public record RefreshTokenRequest(String refreshToken) {
+
+}
---
+++ src/main/java/finalproject/ss3v2/dao/request/SignInRequest.java
@@ -0,0 +1,5 @@
+package finalproject.ss3v2.dao.request;
+
+public record SignInRequest(String email, String password) {
+
+}
