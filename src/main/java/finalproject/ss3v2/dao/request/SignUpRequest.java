d588ddc90c8041875c204cf2fa9116eaa0a2f834
Ezequiel <97308027+Zeky84@users.noreply.github.com>
2024-01-28 20:54:32 -0500

    Initial commit

---
+++ src/main/java/finalproject/ss3v2/dao/request/SignUpRequest.java
@@ -0,0 +1,11 @@
+package finalproject.ss3v2.dao.request;
+
+import java.util.Optional;
+
+public record SignUpRequest(String email,
+                            String password,
+                            String firstName,
+                            String lastName,
+                            Optional<String> authorityOpt) {
+
+}
