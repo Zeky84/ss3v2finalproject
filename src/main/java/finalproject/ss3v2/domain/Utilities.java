5012e6cb58dcd34cfc8d43ee14d0017c99abe0d0
Ezequiel <97308027+Zeky84@users.noreply.github.com>
2024-08-25 22:52:56 -0400

    Removed lombok annotations from Utilities entities, to avoid auto generation of equals(), and hashcode() for all fields

--- src/main/java/finalproject/ss3v2/domain/Utilities.java
+++ src/main/java/finalproject/ss3v2/domain/Utilities.java
@@ -9,11 +9,6 @@ import lombok.experimental.SuperBuilder;
 
 @Entity
 @Table(name = "utilities_cost_by_state")
-@AllArgsConstructor// from lombok
-@NoArgsConstructor // from lombok
-@Data // from lombok
-@EqualsAndHashCode // from lombok
-@SuperBuilder // from lombok
 public class Utilities {
     // This Db will store the values from the .csv file. In this Db aren't included the rent cost and the electricity
     // rate cause these values are going to be provided by APIs calls.
@@ -32,4 +27,99 @@ public class Utilities {
     private double monthlyWasteCost;
     private double publicTransportationCost;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getStateCode() {
+        return stateCode;
+    }
+
+    public void setStateCode(String stateCode) {
+        this.stateCode = stateCode;
+    }
+
+    public String getStateName() {
+        return stateName;
+    }
+
+    public void setStateName(String stateName) {
+        this.stateName = stateName;
+    }
+
+    public double getGasRegularCost() {
+        return gasRegularCost;
+    }
+
+    public void setGasRegularCost(double gasRegularCost) {
+        this.gasRegularCost = gasRegularCost;
+    }
+
+    public double getGasMidGradeCost() {
+        return gasMidGradeCost;
+    }
+
+    public void setGasMidGradeCost(double gasMidGradeCost) {
+        this.gasMidGradeCost = gasMidGradeCost;
+    }
+
+    public double getGasPremiumCost() {
+        return gasPremiumCost;
+    }
+
+    public void setGasPremiumCost(double gasPremiumCost) {
+        this.gasPremiumCost = gasPremiumCost;
+    }
+
+    public double getGasDieselCost() {
+        return gasDieselCost;
+    }
+
+    public void setGasDieselCost(double gasDieselCost) {
+        this.gasDieselCost = gasDieselCost;
+    }
+
+    public double getMonthlyWaterCost() {
+        return monthlyWaterCost;
+    }
+
+    public void setMonthlyWaterCost(double monthlyWaterCost) {
+        this.monthlyWaterCost = monthlyWaterCost;
+    }
+
+    public double getMonthlyInternetCost() {
+        return monthlyInternetCost;
+    }
+
+    public void setMonthlyInternetCost(double monthlyInternetCost) {
+        this.monthlyInternetCost = monthlyInternetCost;
+    }
+
+    public double getMonthlyNaturalGasCost() {
+        return monthlyNaturalGasCost;
+    }
+
+    public void setMonthlyNaturalGasCost(double monthlyNaturalGasCost) {
+        this.monthlyNaturalGasCost = monthlyNaturalGasCost;
+    }
+
+    public double getMonthlyWasteCost() {
+        return monthlyWasteCost;
+    }
+
+    public void setMonthlyWasteCost(double monthlyWasteCost) {
+        this.monthlyWasteCost = monthlyWasteCost;
+    }
+
+    public double getPublicTransportationCost() {
+        return publicTransportationCost;
+    }
+
+    public void setPublicTransportationCost(double publicTransportationCost) {
+        this.publicTransportationCost = publicTransportationCost;
+    }
 }
